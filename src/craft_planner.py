2192506af85074d1a71bde72ee774804c2aec0d3
Jeffrey Zhang <jeffreyzhang2@gmail.com>
2020-07-09 21:53:16 -0700

    added length and add gitignore

--- src/craft_planner.py
+++ src/craft_planner.py
@@ -140,6 +140,7 @@ def search(graph, state, is_goal, limit, heuristic):
     cost_so_far[state] = 0
     actions[state] = None
     path = []
+    length = 0
 
     # Implement your search here! Use your heuristic here!
     # When you find a path to the goal return a list of tuples [(state, action)]
@@ -149,16 +150,19 @@ def search(graph, state, is_goal, limit, heuristic):
         current_cost, current_state = heappop(queue)
 
         if is_goal(current_state):
+            length += 1
             link = (current_state, actions[current_state])
             path.append(link)
             current_state = came_from[current_state]
 
             while (current_state is not None):
+                length += 1
                 link = (current_state, actions[current_state])
                 path.append(link)
                 current_state = came_from[current_state]
             # print('states visited: ' + str(len(visited_states)))
             print(time() - start_time, "seconds.")
+            print("Length is " + str(length))
             path.reverse()
             return path
 
@@ -167,7 +171,7 @@ def search(graph, state, is_goal, limit, heuristic):
             new_cost = cost_so_far[current_state] + adj_cost
             if adj_state not in cost_so_far or new_cost < cost_so_far[adj_state]:
                 cost_so_far[adj_state] = new_cost
-                priority = new_cost + heuristic(state)
+                priority = new_cost + heuristic(adj_state)
                 actions[adj_state] = adj_action
                 heappush(queue, (priority, adj_state))
                 came_from[adj_state] = current_state
